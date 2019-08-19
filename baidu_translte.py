3e6f21e78399df052651284b9eec571fab417242
tpzaza77 <50908105+tpzaza77@users.noreply.github.com>
2019-08-19 10:21:28 +0800

    Add files via upload

---
+++ baidu_translte.py
@@ -0,0 +1,65 @@
+import requests
+import re
+import execjs
+from fake_useragent import UserAgent
+
+
+class BaiduTranslateSpider:
+    def __init__(self):
+        self.get_url = 'https://fanyi.baidu.com/?aldtype=16047'
+        self.headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+            # 'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cache-control': 'no-cache',
+            'cookie': 'BAIDUID=52920E829C1F64EE98183B703F4E37A9:FG=1; BIDUPSID=52920E829C1F64EE98183B703F4E37A9; PSTM=1562657403; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1; delPer=0; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; BCLID=6890774803653935935; BDSFRCVID=4XAsJeCCxG3DLCbwbJrKDGwjNA0UN_I3KhXZ3J; H_BDCLCKID_SF=tRk8oIDaJCvSe6r1MtQ_M4F_qxby26nUQ5neaJ5n0-nnhnL4W46bqJKFLtozKMoI3C7fotJJ5nololIRy6CKjjb-jaDqJ5n3bTnjstcS2RREHJrg-trSMDCShGRGWlO9WDTm_D_KfxnkOnc6qJj0-jjXqqo8K5Ljaa5n-pPKKRAaqD04bPbZL4DdMa7HLtAO3mkjbnczfn02OP5P5lJ_e-4syPRG2xRnWIvrKfA-b4ncjRcTehoM3xI8LNj405OTt2LEoDPMJKIbMI_rMbbfhKC3hqJfaI62aKDs_RCMBhcqEIL4eJOIb6_w5gcq0T_HttjtXR0atn7ZSMbSj4Qo5pK95p38bxnDK2rQLb5zah5nhMJS3j7JDMP0-4rJhxby523i5J6vQpnJ8hQ3DRoWXPIqbN7P-p5Z5mAqKl0MLIOkbC_6j5DWDTvLeU7J-n8XbI60XRj85-ohHJrFMtQ_q4tehHRMBUo9WDTm_DoTttt5fUj6qJj855jXqqo8KMtHJaFf-pPKKRAashnzWjrkqqOQ5pj-WnQr3mkjbn5yfn02OpjPX6joht4syPRG2xRnWIvrKfA-b4ncjRcTehoM3xI8LNj405OTt2LEoC0XtIDhMDvPMCTSMt_HMxrKetJyaR0JhpjbWJ5TEPnjDUOdLPDW-46HBM3xbKQw5CJGBf7zhpvdWhC5y6ISKx-_J68Dtf5; ZD_ENTRY=baidu; PSINO=2; H_PS_PSSID=26525_1444_21095_29578_29521_28518_29098_29568_28830_29221_26350_29459; locale=zh; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1563426293,1563996067; from_lang_often=%5B%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1563999768; yjs_js_security_passport=2706b5b03983b8fa12fe756b8e4a08b98fb43022_1563999769_js',
+            'pragma': 'no-cache',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
+        }
+    #获取token
+    def get_token(self):
+        html = requests.get(
+            url = self.get_url,
+            headers = self.headers
+        ).text
+        #正则解析
+        pattern = re.compile("token: '(.*?)'",re.S)
+        token = pattern.findall(html)[0]
+        return token
+
+    #获取sign
+    def get_sign(self,word):
+        with open('text.js','r') as f:
+            js_data = f.read()
+        execjs_obj = execjs.compile(js_data)
+        sign = execjs_obj.eval('e("{}")'.format(word))
+        return sign
+
+    #获取翻译结果
+    def get_result(self,word):
+        token = self.get_token()
+        sign = self.get_sign(word)
+        #return result
+        formdata = {
+            'from': 'en',
+            'to': 'zh',
+            'query': word,
+            'transtype': 'realtime',
+            'simple_means_flag': '3',
+            'sign': sign,
+            'token': token,
+        }
+        html_json = requests.post(
+            url = 'https://fanyi.baidu.com/v2transapi',
+            data=formdata,
+            headers = self.headers
+        ).json()
+        print(html_json['trans_result']['data'][0]['dst'])
+
+if __name__ == '__main__':
+    spider = BaiduTranslateSpider()
+    word = input("请输入要翻译的单词")
+    #result = spider.get_result(word)
+    spider.get_result(word)
+    #print(spider.get_token())
